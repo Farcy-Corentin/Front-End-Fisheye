08e267aa3e00466c57020a10c8587ffc38bd9ce5
corentin <corentinfarcy1@gmail.com>
2022-11-10 11:02:29 +0100

    fetch photographer data add eslint/prettier

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const articleEl = document.createElement('article')
+
+    const portraitEl = document.createElement('img')
+    portraitEl.setAttribute('src', picture)
+
+    const nameEl = document.createElement('h2')
+    nameEl.textContent = name
+
+    const cityWithCountryEl = document.createElement('p')
+    cityWithCountryEl.textContent = `${city}, ${country}`
+
+    const taglineEl = document.createElement('p')
+    taglineEl.textContent = tagline
+
+    const priceEl = document.createElement('p')
+    priceEl.textContent = `${price}€/jour`
+
+    articleEl.appendChild(portraitEl)
+    articleEl.appendChild(nameEl)
+    articleEl.appendChild(cityWithCountryEl)
+    articleEl.appendChild(taglineEl)
+    articleEl.appendChild(priceEl)
+    return articleEl
+  }
+  return { name, picture, getUserCardDOM }
 }
